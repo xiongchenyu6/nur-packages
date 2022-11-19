188b5f1c02b2ccebe8ef624b6b6daaa6a533bb29
freeman <xiongchenyu6@gmail.com>
2022-11-20 04:20:38 +0800

    Add: empty templete

---
+++ templates/empty/flake.nix
@@ -0,0 +1,30 @@
+# SPDX-FileCopyrightText: 2021 Serokell <https://serokell.io/>
+#
+# SPDX-License-Identifier: CC0-1.0
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+
+  };
+
+  outputs = { self, nixpkgs, flake-utils, devshell, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ devshell.overlay ];
+          config.allowUnfree = true;
+        };
+      in {
+        devShell =
+          pkgs.devshell.mkShell { commands = [{ package = pkgs.python2; }]; };
+      });
+}
--- templates/java/flake.nix
+++ templates/java/flake.nix
@@ -5,16 +5,58 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, devshell, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShell = pkgs.mkShell {
-          buildInputs = [ pkgs.openjdk8 ];
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ devshell.overlay ];
+          config.allowUnfree = true;
+        };
+      in {
+        packages.default = pkgs.callPackage ./default.nix { };
+        devShell = pkgs.devshell.mkShell {
+          env = [{
+            name = "JAVA_HOME";
+            value = "${pkgs.openjdk11}";
+          }];
+          commands = [
+            {
+              category = "Programming language support";
+              package = pkgs.openjdk11;
+              help = ''
+                1. use gradle assemble to install dependency first
+                              2. use gradle build -x test for dev build 
+                              3. go to node/build/lib to find jars 
+              '';
+            }
+            {
+              category = "Java package manager";
+              package = pkgs.gradle.override { java = pkgs.openjdk11; };
+              name = "gradle";
+              help = ''
+                1. use gradle assemble to install dependency first
+                              2. use gradle build -x test for dev build 
+                              3. go to node/build/lib to find jars 
+              '';
+            }
+            {
+              category = "Java package manager";
+              package = pkgs.nodejs-14_x;
+              name = "nodejs";
+            }
+            { package = pkgs.python2; }
+          ];
         };
       });
 }
