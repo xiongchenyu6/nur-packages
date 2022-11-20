938f17b3ecabfc3f75989df2ae1c789b18b77dc8
freeman <xiongchenyu6@gmail.com>
2022-11-20 16:22:00 +0800

    Add: python template

--- templates/default.nix
+++ templates/default.nix
@@ -7,4 +7,8 @@
     path = ./java;
     description = "A grossly incandescent and minimal nixos java config";
   };
+  python = {
+    path = ./python;
+    description = "A grossly incandescent and minimal nixos java config";
+  };
 }
