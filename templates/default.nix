fbe47c56214384afff748f0c9bf97fedce36e992
freeman <xiongchenyu6@gmail.com>
2022-11-20 16:08:11 +0800

    Add: empty template

--- templates/default.nix
+++ templates/default.nix
@@ -1,4 +1,8 @@
 {
+  default = {
+    path = ./empty;
+    description = "A grossly incandescent and minimal nixos empty config";
+  };
   java = {
     path = ./java;
     description = "A grossly incandescent and minimal nixos java config";
