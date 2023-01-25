6e582e0cfb1cf6ddddf609ff34bfa888ac5c4049
freeman <xiongchenyu6@gmail.com>
2023-01-25 16:06:07 +0800

    Feature: add nixops fixed

--- default.nix
+++ default.nix
@@ -20,98 +20,100 @@ let
   callPackage = lib.callPackageWith allPkgs;
   my-pkgs = rec {
     # example-docker =
-    #   pkgs.dockerTools.buildImage {
-    #     name = "hello-docker";
-    #     tag = "latest";
-    #     created = "now";
-    #     runAsRoot = ''
-    #       mkdir /data
-    #     '';
-    #     copyToRoot = pkgs.buildEnv {
-    #       name = "image-root";
-    #       paths = [
-    #         pkgs.coreutils
-    #         pkgs.bash
-    #         pkgs.vim
-    #       ];
-    #       pathsToLink = [ "/bin" ];
-    #     };
+      #   pkgs.dockerTools.buildImage {
+        #     name = "hello-docker";
+        #     tag = "latest";
+        #     created = "now";
+        #     runAsRoot = ''
+        #       mkdir /data
+        #     '';
+        #     copyToRoot = pkgs.buildEnv {
+          #       name = "image-root";
+          #       paths = [
+            #         pkgs.coreutils
+            #         pkgs.bash
+            #         pkgs.vim
+            #       ];
+            #       pathsToLink = [ "/bin" ];
+            #     };
 
-    #     config = {
-    #       WorkingDir = "/data";
-    #       Env = [ "PATH=${pkgs.coreutils}/bin/" ];
-    #       Cmd = [ "${pkgs.coreutils}/bin/cat" "${my-pkgs.example-package}" ];
-    #     };
-    #   };
+            #     config = {
+              #       WorkingDir = "/data";
+              #       Env = [ "PATH=${pkgs.coreutils}/bin/" ];
+              #       Cmd = [ "${pkgs.coreutils}/bin/cat" "${my-pkgs.example-package}" ];
+              #     };
+              #   };
 
-    launch = stdenv.mkDerivation (source.launch // {
-      installPhase = ''
-        mkdir -p $out;
-        cp -r . $out;
-      '';
-    });
+              launch = stdenv.mkDerivation (source.launch // {
+                installPhase = ''
+                  mkdir -p $out;
+                  cp -r . $out;
+                '';
+              });
 
-    bttc = callPackage ./pkgs/bttc { };
-    delivery = callPackage ./pkgs/delivery { };
-    marksman = callPackage ./pkgs/marksman { };
+              bttc = callPackage ./pkgs/bttc { };
+              delivery = callPackage ./pkgs/delivery { };
+              marksman = callPackage ./pkgs/marksman { };
 
-    oci-arm-host-capacity = (inputs.dream2nix.lib.makeFlakeOutputs {
-      pkgs = inputs.dream2nix.inputs.nixpkgs.legacyPackages."x86_64-linux";
-      source = inputs.oci-arm-host-capacity-src;
-      config.projectRoot = ./.;
-      autoProjects = true;
-    }).packages."x86_64-linux"."hitrov/oci-arm-host-capacity";
+              oci-arm-host-capacity = (inputs.dream2nix.lib.makeFlakeOutputs {
+                pkgs = inputs.dream2nix.inputs.nixpkgs.legacyPackages."x86_64-linux";
+                source = inputs.oci-arm-host-capacity-src;
+                config.projectRoot = ./.;
+                autoProjects = true;
+              }).packages."x86_64-linux"."hitrov/oci-arm-host-capacity";
 
-    my_cookies = callPackage ./pkgs/python3/my_cookies { };
-    epc = callPackage ./pkgs/python3/epc { };
-    pem = callPackage ./pkgs/python3/pem { };
-    newsapi-python = callPackage ./pkgs/python3/newsapi-python { };
+              my_cookies = callPackage ./pkgs/python3/my_cookies { };
+              epc = callPackage ./pkgs/python3/epc { };
+              pem = callPackage ./pkgs/python3/pem { };
+              newsapi-python = callPackage ./pkgs/python3/newsapi-python { };
 
-    django-ordered-model = callPackage ./pkgs/python3/django-ordered-model { };
-    django-sns-view = callPackage ./pkgs/python3/django-sns-view { };
+              django-ordered-model = callPackage ./pkgs/python3/django-ordered-model { };
+              django-sns-view = callPackage ./pkgs/python3/django-sns-view { };
 
-    Flask-SimpleLDAP = callPackage ./pkgs/python3/Flask-SimpleLDAP { };
-    # lsp-bridge = callPackage ./pkgs/emacs/lsp-bridge { };
+              Flask-SimpleLDAP = callPackage ./pkgs/python3/Flask-SimpleLDAP { };
+              # lsp-bridge = callPackage ./pkgs/emacs/lsp-bridge { };
 
-    copilot-el = callPackage ./pkgs/emacs/copilot { };
+              copilot-el = callPackage ./pkgs/emacs/copilot { };
 
-    ligature = callPackage ./pkgs/emacs/ligature { };
+              ligature = callPackage ./pkgs/emacs/ligature { };
 
-    org-cv = callPackage ./pkgs/emacs/org-cv { };
+              org-cv = callPackage ./pkgs/emacs/org-cv { };
 
-    inherit (callPackage ./pkgs/npm/tronbox { }) tronbox;
+              inherit (callPackage ./pkgs/npm/tronbox { }) tronbox;
 
-    inherit (callPackage ./pkgs/npm/solium { }) solium;
+              inherit (callPackage ./pkgs/npm/solium { }) solium;
 
-    amazon-cloudwatch-agent = callPackage ./pkgs/amazon-cloudwatch-agent { };
+              amazon-cloudwatch-agent = callPackage ./pkgs/amazon-cloudwatch-agent { };
 
-    # vbox = nixos-generators.nixosGenerate {
-    #   inherit system;
-    #   format = "virtualbox";
-    # };
-    # amazon = nixos-generators.nixosGenerate {
-    #   system = "x86_64-linux";
-    #   format = "amazon";
-    # };
+              nixops-fixed = callPackage ./pkgs/nixops-fixed { };
 
-    # tat = callPackage ./tat { };
+              # vbox = nixos-generators.nixosGenerate {
+                #   inherit system;
+                #   format = "virtualbox";
+                # };
+                # amazon = nixos-generators.nixosGenerate {
+                  #   system = "x86_64-linux";
+                  #   format = "amazon";
+                  # };
 
-    # dotfiles = with pkgs;
-    #   stdenv.mkDerivation {
-    #     pname = "dotfiles";
-    #     version = "0.1.0";
-    #     src = ./.;
-    #     installPhase = ''
-    #       mkdir -p $out/etc;
-    #       cp -r . $out/etc;
-    #     '';
-    #   };
-    ldap-passthrough-conf = callPackage ./pkgs/ldap-passthrough-conf { };
+                  # tat = callPackage ./tat { };
 
-    ldap-extra-schemas = callPackage ./pkgs/ldap-extra-schemas { };
+                  # dotfiles = with pkgs;
+                  #   stdenv.mkDerivation {
+                    #     pname = "dotfiles";
+                    #     version = "0.1.0";
+                    #     src = ./.;
+                    #     installPhase = ''
+                    #       mkdir -p $out/etc;
+                    #       cp -r . $out/etc;
+                    #     '';
+                    #   };
+                    ldap-passthrough-conf = callPackage ./pkgs/ldap-passthrough-conf { };
 
-    codedeploy-agent = callPackage ./pkgs/ruby/codedeploy-agent { };
-    default = bttc;
-    # };
+                    ldap-extra-schemas = callPackage ./pkgs/ldap-extra-schemas { };
+
+                    codedeploy-agent = callPackage ./pkgs/ruby/codedeploy-agent { };
+                    default = bttc;
+                    # };
   };
 in my-pkgs
