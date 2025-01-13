c2a4e06a12a1c2d0ea95dd69e43a8f3daf892826
xiongchenyu <xiongchenyu6@gmail.com>
2025-01-13 19:14:44 +0800

    update nixos template

--- templates/default.nix
+++ templates/default.nix
@@ -15,27 +15,22 @@
     path = ./python;
     description = "A grossly incandescent and minimal nixos python config";
   };
-
   rust = {
     path = ./rust;
     description = "A grossly incandescent and minimal nixos rust config";
   };
-
   cc = {
     path = ./cc;
     description = "A grossly incandescent and minimal nixos cpp config";
   };
-
   go = {
     path = ./go;
     description = "A grossly incandescent and minimal nixos go config";
   };
-
   shell = {
     path = ./shell;
     description = "A grossly incandescent and minimal nixos shell config";
   };
-
   cuda = {
     path = ./cuda;
     description = "A grossly incandescent and minimal nixos cuda config";
@@ -43,6 +38,10 @@
   terraform = {
     path = ./terraform;
     description = "A grossly incandescent and minimal nixos terraform config";
+  };
+  nixos = {
+    path = ./nixos;
+    description = "A grossly incandescent and minimal nixos nixos config";
   };
 
 }
