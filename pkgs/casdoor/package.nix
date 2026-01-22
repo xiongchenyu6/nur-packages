f5ed31128d6efce72fbfde73799f240e9dd36883
xiongchenyu <xiongchenyu6@gmail.com>
2026-01-22 10:29:31 +0800

    casdoor: add multi-platform support (x86_64/aarch64 for Linux/Darwin)

--- pkgs/casdoor/package.nix
+++ pkgs/casdoor/package.nix
@@ -6,29 +6,39 @@
 }:
 let
   version = "2.253.0";
-  passthru.platforms = [
-    "x86_64-linux"
-    "aarch64-linux"
-    "x86_64-darwin"
-    "aarch64-darwin"
-  ];
+
+  sources = {
+    "x86_64-linux" = {
+      url = "https://github.com/casdoor/casdoor/releases/download/v${version}/casdoor_Linux_x86_64.tar.gz";
+      hash = "sha256-quPcr7Ff9D077fp9gUz50/D1+Ejz0AO4cSwQzDW3bEA=";
+    };
+    "aarch64-linux" = {
+      url = "https://github.com/casdoor/casdoor/releases/download/v${version}/casdoor_Linux_arm64.tar.gz";
+      hash = "sha256-/EMRJUK4R9ZeWUGRsY7+cxclEo+/nSzn+Q8WsUE9Ydk=";
+    };
+    "x86_64-darwin" = {
+      url = "https://github.com/casdoor/casdoor/releases/download/v${version}/casdoor_Darwin_x86_64.tar.gz";
+      hash = "sha256-KBJ037k3/w084PZ3P1Ous5lO/ABmEVOscn5N4BDpVe8=";
+    };
+    "aarch64-darwin" = {
+      url = "https://github.com/casdoor/casdoor/releases/download/v${version}/casdoor_Darwin_arm64.tar.gz";
+      hash = "sha256-V1Anymw638S1YocuLSXZVNOpJgiueIoi8Q/b7hmbh2s=";
+    };
+  };
 in
 stdenv.mkDerivation {
   pname = "casdoor";
   inherit version;
 
-  src = fetchurl {
-    url = "https://github.com/casdoor/casdoor/releases/download/v${version}/casdoor_Linux_x86_64.tar.gz";
-    hash = "sha256-quPcr7Ff9D077fp9gUz50/D1+Ejz0AO4cSwQzDW3bEA=";
-  };
+  src = fetchurl sources.${stdenv.hostPlatform.system};
 
   sourceRoot = ".";
 
-  nativeBuildInputs = [
+  nativeBuildInputs = lib.optionals stdenv.hostPlatform.isLinux [
     autoPatchelfHook
   ];
 
-  buildInputs = [
+  buildInputs = lib.optionals stdenv.hostPlatform.isLinux [
     stdenv.cc.cc
   ];
 
@@ -46,7 +56,7 @@ stdenv.mkDerivation {
     description = "A Django-like Go/React login web UI that supports OAuth, OIDC, SAML, CAS, LDAP, etc.";
     homepage = "https://casdoor.org";
     license = licenses.asl20;
-    platforms = platforms.unix;
+    platforms = builtins.attrNames sources;
     maintainers = [ "freeman" ];
   };
 }
