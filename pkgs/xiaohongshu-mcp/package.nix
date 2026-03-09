2e931c1f5b254e321ec8f7ded3fb499adedbf621
xiongchenyu <xiongchenyu6@gmail.com>
2026-03-09 21:17:35 +0800

    chore: update packages via nvfetcher

--- pkgs/xiaohongshu-mcp/package.nix
+++ pkgs/xiaohongshu-mcp/package.nix
@@ -5,28 +5,28 @@
 }:
 
 let
-  version = "v2026.03.04.0231-db81fd8";
+  version = "v2026.03.09.0605-0e16f4b";
 
   sources = {
     x86_64-linux = {
       osArch = "linux-amd64";
       url = "https://github.com/xpzouying/xiaohongshu-mcp/releases/download/${version}/xiaohongshu-mcp-linux-amd64.tar.gz";
-      hash = "sha256-Im3h0923bdLtNQI3sw9gp3k81hTacRpL8itRhc7qavk=";
+      hash = "sha256-T8CWYRffhG+GpUmJWn0X8nwQbT7dRwUCXXyUX4MfAwA=";
     };
     aarch64-linux = {
       osArch = "linux-arm64";
       url = "https://github.com/xpzouying/xiaohongshu-mcp/releases/download/${version}/xiaohongshu-mcp-linux-arm64.tar.gz";
-      hash = "sha256-TULMiB7+y4A7FEApD3b5Ng0H5sBABd5yixX+YqqIlmU=";
+      hash = "sha256-enM/uW4Fk5eqtTGH1MXmGk+HrIXbRWh3uDszNCVhY2s=";
     };
     x86_64-darwin = {
       osArch = "darwin-amd64";
       url = "https://github.com/xpzouying/xiaohongshu-mcp/releases/download/${version}/xiaohongshu-mcp-darwin-amd64.tar.gz";
-      hash = "sha256-7flQ6aKP7B6AlJ3ayhvrj7wBmfaK90/dr60Ckmjvy34=";
+      hash = "sha256-jec2Q/wV8E7xFRnGZJNNkgWnIZj4pTu3c7xZKXWKH58=";
     };
     aarch64-darwin = {
       osArch = "darwin-arm64";
       url = "https://github.com/xpzouying/xiaohongshu-mcp/releases/download/${version}/xiaohongshu-mcp-darwin-arm64.tar.gz";
-      hash = "sha256-wo+CabtKEje47UfVDoYr7PaHmo0UwHljlxcis8NAZx0=";
+      hash = "sha256-LR1LO8avMv2e1UbDkwnUJd0suaZBW+LmZSXNmjso/gU=";
     };
   };
 
