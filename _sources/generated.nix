86714ce23fa6f33d8bbd900c33d5ace54e752aee
xiongchenyu6 <4978864+xiongchenyu6@users.noreply.github.com>
2026-09-01 04:51:04 +0000

    chore: update flake inputs and nvfetcher sources

--- _sources/generated.nix
+++ _sources/generated.nix
@@ -284,13 +284,13 @@
   };
   sub2api = {
     pname = "sub2api";
-    version = "v0.1.184";
+    version = "v0.1.185";
     src = fetchFromGitHub {
       owner = "Wei-Shaw";
       repo = "sub2api";
-      rev = "v0.1.184";
+      rev = "v0.1.185";
       fetchSubmodules = false;
-      sha256 = "sha256-0Hto/LmJKKfWOa3Rq9crK15pOorQkwyFRMc0iyt2TbE=";
+      sha256 = "sha256-6603gIrlClgBI/3Yu1fOmNKLyaHISsomCLGIJ0FoTeI=";
     };
   };
   sui = {
